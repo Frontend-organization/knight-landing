fd0f92c765736248693d544309f62bd0f54f108c
Manuel <m.garcia@rpalatam.com.pe>
2020-10-03 00:56:00 -0500

    Feat: section benefits added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Benefits from "./components/Benefits";
 import Home from "./components/Home";
 
 const App = () => {
   return (
     <React.Fragment>
       <Home />
+      <Benefits />
     </React.Fragment>
   );
 };
